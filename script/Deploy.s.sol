ea3c9682f9f07473093800ae1beac8af0028a4ee
erhant <erhany96@gmail.com>
2024-12-18 17:43:20 +0300

    update swan

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -7,31 +7,9 @@ import {Script} from "forge-std/Script.sol";
 import {Vm} from "forge-std/Vm.sol";
 import {HelperConfig} from "./HelperConfig.s.sol";
 
-import {LLMOracleRegistry} from "@firstbatch/dria-oracle-contracts/LLMOracleRegistry.sol";
-import {
-    LLMOracleCoordinator, LLMOracleTaskParameters
-} from "@firstbatch/dria-oracle-contracts/LLMOracleCoordinator.sol";
 import {SwanAgentFactory} from "../src/SwanAgent.sol";
 import {SwanArtifactFactory} from "../src/SwanArtifact.sol";
 import {Swan, SwanMarketParameters} from "../src/Swan.sol";
-
-contract DeployLLMOracleRegistry is Script {
-    HelperConfig public config;
-
-    function run() external returns (address proxy, address impl) {
-        config = new HelperConfig();
-        (proxy, impl) = config.deployLLMOracleRegistry();
-    }
-}
-
-contract DeployLLMOracleCoordinator is Script {
-    HelperConfig public config;
-
-    function run() external returns (address proxy, address impl) {
-        config = new HelperConfig();
-        (proxy, impl) = config.deployLLMOracleCoordinator();
-    }
-}
 
 contract DeploySwanAgentFactory is Script {
     HelperConfig public config;
