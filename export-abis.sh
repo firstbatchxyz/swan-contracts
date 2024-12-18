983368d1f26dba58bb24695d740c1208c0c04f69
erhant <erhany96@gmail.com>
2024-12-18 14:40:31 +0300

    added abi exporter

---
+++ export-abis.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+mkdir abis
+
+cp ./out/Swan.sol/Swan.json ./abis/Swan.json
+node ./abis/parseAbi.js ./abis/Swan.json
+
+cp ./out/SwanAgent.sol/SwanAgent.json ./abis/SwanAgent.json
+node ./abis/parseAbi.js ./abis/SwanAgent.json
+
+cp ./out/SwanArtifact.sol/SwanArtifact.json ./abis/SwanArtifact.json
+node ./abis/parseAbi.js ./abis/SwanArtifact.json
