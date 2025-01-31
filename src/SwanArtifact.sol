e55a734082a29f3da74b03f5e2e67dbeb13c4aee
vivinvinh212 <vinh@cega.fi>
2025-01-31 11:56:48 +0700

    SwanArtifact: Undo changes

--- src/SwanArtifact.sol
+++ src/SwanArtifact.sol
@@ -20,47 +20,21 @@ contract SwanArtifactFactory {
 contract SwanArtifact is ERC721, Ownable {
     /// @notice Creation time of the token
     uint256 public createdAt;
-
     /// @notice Description of the token
     bytes public description;
 
-    /// @notice Swan operator address that cannot have its approval revoked
-    address public immutable swanOperator;
-
-    /// @notice Error thrown when attempting to revoke Swan's approval
-    error CannotRevokeSwan();
-
     /// @notice Constructor sets properties of the token.
-    constructor(
-        string memory _name,
-        string memory _symbol,
-        bytes memory _description,
-        address _owner,
-        address _operator
-    ) ERC721(_name, _symbol) Ownable(_owner) {
+    constructor(string memory _name, string memory _symbol, bytes memory _description, address _owner)
+        ERC721(_name, _symbol)
+        Ownable(_owner)
+    {
         description = _description;
         createdAt = block.timestamp;
-        swanOperator = _operator;
 
         // owner is minted the token immediately
-        _safeMint(_owner, 1);
+        ERC721._safeMint(_owner, 1);
 
         // Swan (operator) is approved to by the owner immediately.
-        _setApprovalForAll(_owner, _operator, true);
-    }
-
-    function setApprovalForAll(address operator, bool approved) public override {
-        if (operator == swanOperator && !approved) {
-            revert CannotRevokeSwan();
-        }
-        super.setApprovalForAll(operator, approved);
-    }
-
-    function approve(address to, uint256 tokenId) public override {
-        address owner = ownerOf(tokenId);
-        if (isApprovedForAll(owner, swanOperator) && to != swanOperator) {
-            revert CannotRevokeSwan();
-        }
-        super.approve(to, tokenId);
+        ERC721._setApprovalForAll(_owner, _operator, true);
     }
 }
